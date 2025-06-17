754f211c451e428964e0cd25c179b4dc68a15210
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2025-06-17 13:46:37 +0200

    Update on host_webrtc_hook removed promise wrapper instead using async

--- frontend/src/lib/webrtc/host_webrtc_hook.ts
+++ frontend/src/lib/webrtc/host_webrtc_hook.ts
@@ -14,6 +14,7 @@ import { exportStunServers } from './stun_servers';
 import { exportTurnServers } from './turn_servers';
 import { isLinux } from '$lib/detection/detect_os';
 import { IS_RUNNING_EXTERNAL } from '$lib/detection/onwebsite';
+import log from '$lib/logger/logger';
 
 const BROWSER_BASE_URL = 'http://localhost:8080/mode/host/connection';
 
@@ -30,64 +31,61 @@ interface CreateHostOptions {
 	clientCode: string;
 }
 
-export function CreateHost(options: CreateHostOptions) {
-	return new Promise<string>((resolve, reject) => {
-		(async () => {
-			try {
-				const { clientCode: client, easyConnect } = options;
+export async function CreateHost(options: CreateHostOptions) {
+	try {
+		const { clientCode: client, easyConnect } = options;
 
-				const ICEServers: ICEServer[] = [...exportStunServers(), ...exportTurnServers()];
+		const ICEServers: ICEServer[] = [...exportStunServers(), ...exportTurnServers()];
 
-				const hostCode = await createHostFn(ICEServers, client);
+		const hostCode = await createHostFn(ICEServers, client);
 
-				if (isError(hostCode)) {
-					reject(hostCode);
-				}
+		if (isError(hostCode)) {
+      throw hostCode;
+		}
 
-				resolve(hostCode);
+		if (!easyConnect && navigator && navigator.clipboard && navigator.clipboard.writeText) {
+			navigator.clipboard.writeText(hostCode).catch(() => {
+				showToast(get(_)('error-copying-host-code-to-clipboard'), ToastType.ERROR);
+			});
+			showToast(get(_)('host-code-copied-to-clipboard'), ToastType.SUCCESS);
+		} else if (!easyConnect) {
+			showToast(get(_)('error-copying-host-code-to-clipboard'), ToastType.ERROR);
+		}
 
-				if (!easyConnect && navigator && navigator.clipboard && navigator.clipboard.writeText) {
-					navigator.clipboard.writeText(hostCode).catch(() => {
-						showToast(get(_)('error-copying-host-code-to-clipboard'), ToastType.ERROR);
-					});
-					showToast(get(_)('host-code-copied-to-clipboard'), ToastType.SUCCESS);
-				} else if (!easyConnect) {
-					showToast(get(_)('error-copying-host-code-to-clipboard'), ToastType.ERROR);
-				}
+		toogleLoading();
+		setLoadingMessage(get(_)('waiting-for-client-to-connect'));
+		setLoadingTitle(get(_)('make-sure-to-pass-the-code-to-the-client'));
 
-				toogleLoading();
-				setLoadingMessage(get(_)('waiting-for-client-to-connect'));
-				setLoadingTitle(get(_)('make-sure-to-pass-the-code-to-the-client'));
+		const { EventsOnce } = await import('$lib/wailsjs/runtime/runtime');
 
-				const { EventsOnce } = await import('$lib/wailsjs/runtime/runtime');
+		EventsOnce('connection_state', async (state: ConnectionState) => {
+			toogleLoading();
 
-				EventsOnce('connection_state', async (state: ConnectionState) => {
-					toogleLoading();
-
-					switch (state.toUpperCase()) {
-						case ConnectionState.Connected:
-							showToast(get(_)('connected'), ToastType.SUCCESS);
-							host = true;
-							if (await isLinux()) {
-								const { BrowserOpenURL } = await import('$lib/wailsjs/runtime/runtime');
-								BrowserOpenURL(BROWSER_BASE_URL);
-							}
-							goto('/mode/host/connection');
-							break;
-						case ConnectionState.Failed:
-							showToast(get(_)('connection-failed'), ToastType.ERROR);
-							goto('/');
-							break;
-						default:
-							showToast(get(_)('unknown-connection-state'), ToastType.ERROR);
-							break;
+			switch (state.toUpperCase()) {
+				case ConnectionState.Connected:
+					showToast(get(_)('connected'), ToastType.SUCCESS);
+					host = true;
+					if (await isLinux()) {
+						const { BrowserOpenURL } = await import('$lib/wailsjs/runtime/runtime');
+						BrowserOpenURL(BROWSER_BASE_URL);
 					}
-				});
-			} catch (e) {
-				showToast(get(_)('error-creating-host'), ToastType.ERROR);
+					goto('/mode/host/connection');
+					break;
+				case ConnectionState.Failed:
+					showToast(get(_)('connection-failed'), ToastType.ERROR);
+					goto('/');
+					break;
+				default:
+					showToast(get(_)('unknown-connection-state'), ToastType.ERROR);
+					break;
 			}
-		})();
-	});
+		});
+		
+		return hostCode
+	} catch (e) {
+    log(e, {err: true})
+		showToast(get(_)('error-creating-host'), ToastType.ERROR);
+	}
 }
 
 function isError(err: string) {
