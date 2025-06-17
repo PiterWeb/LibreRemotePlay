85e39b8c68a7e260b1c58a19d486c1c9097602ea
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2025-06-17 14:13:23 +0200

    Catch error on logger

--- frontend/src/lib/logger/logger.ts
+++ frontend/src/lib/logger/logger.ts
@@ -4,7 +4,7 @@ export default async function log(info: unknown, { err } = { err: false }) {
 	try {
 		const { LogPrintln } = await import("$lib/wailsjs/go/bindings/App");
 		await LogPrintln(`Browser - ${JSON.stringify(info)}\n`);
-	} finally {
+  } catch {/* */} finally {
 		if (err) console.error(err);
 		else console.log(info);
 	}
