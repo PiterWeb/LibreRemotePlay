6f2b2b5ca1ca3626ddaac5dd8cce9137a961a6cb
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2025-06-18 22:08:30 +0200

    Tooltip component created

---
+++ frontend/src/lib/layout/Tooltip.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+  import {
+  	FloatingArrow,
+  	arrow,
+  	autoUpdate,
+  	flip,
+  	offset,
+  	useDismiss,
+  	useFloating,
+  	useHover,
+  	useInteractions,
+  	useRole,
+		type UseFloatingOptions,
+  } from "@skeletonlabs/floating-ui-svelte";
+	import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+  
+  interface TooltipProps {
+    children: import('svelte').Snippet,
+    ref: string,
+    placement?: UseFloatingOptions["placement"]
+  }
+  
+  let { children, ref, placement }: TooltipProps = $props();
+  
+  // State
+  let open = $state(false);
+  let elemArrow: HTMLElement | null = $state(null);
+  
+  // Use Floating
+  const floating = useFloating({
+  	whileElementsMounted: autoUpdate,
+  	get open() {
+  		return open;
+  	},
+  	onOpenChange: (v) => {
+  		open = v;
+  	},
+  	placement: placement ?? "top",
+  	get middleware() {
+  		return [offset(10), flip(), elemArrow && arrow({ element: elemArrow })];
+  	},
+  });
+  
+  onMount(() => {
+    const elementForTooltip = document.getElementById(ref)
+    const referenceProps = interactions.getReferenceProps()
+    
+    for (const [key, value] of Object.entries(referenceProps)) {
+      if (key.startsWith('on') && typeof value === 'function') {
+        // Events like onClick, onMouseEnter...
+        const eventName = key.slice(2).toLowerCase();
+        elementForTooltip?.addEventListener(eventName as keyof HTMLElementEventMap, value as EventListener);
+      } else {
+        // Atributes like aria-label, id, role, etc.
+        elementForTooltip?.setAttribute(key, value as string);
+      }
+    }
+    
+    floating.elements.reference = document.getElementById(ref)
+  })
+  
+  // Interactions
+  const role = useRole(floating.context, { role: "tooltip" });
+  const hover = useHover(floating.context, { move: false });
+  const dismiss = useDismiss(floating.context);
+  const interactions = useInteractions([role, hover, dismiss]);
+</script>
+
+<!-- Floating Element -->
+{#if open}
+	<div
+		bind:this={floating.elements.floating}
+		style={floating.floatingStyles}
+		{...interactions.getFloatingProps()}
+		class="floating popover-neutral"
+		transition:fade={{ duration: 200 }}
+	>
+        <div class="dark:bg-gray-800 dark:border-gray-700 border-2 rounded-lg p-4 text-white">
+            {@render children?.()}
+        </div>
+		<FloatingArrow bind:ref={elemArrow} context={floating.context} fill="#575969" />
+	</div>
+{/if}
