6fe876b2d3cf8e4cd4771eae02a117b32a4a3cdf
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2025-10-12 14:45:14 +0200

    keyMap created as global to not allocate a map in every key change

--- src/devices/keyboard/map_js_keys_go.go
+++ src/devices/keyboard/map_js_keys_go.go
@@ -2,22 +2,23 @@ package keyboard
 
 import "strings"
 
+var keyMap = map[string]string{
+	"Enter":      "enter",
+	"Escape":     "esc",
+	"Backspace":  "backspace",
+	"Tab":        "tab",
+	" ":          "space",
+	"ArrowUp":    "up",
+	"ArrowDown":  "down",
+	"ArrowLeft":  "left",
+	"ArrowRight": "right",
+	"Shift":      "shift",
+	"Control":    "ctrl",
+	"Alt":        "alt",
+	"CapsLock":   "capslock",
+}
+
 func mapJSKeyToRobotGo(jsKey string) string {
-	keyMap := map[string]string{
-		"Enter":      "enter",
-		"Escape":     "esc",
-		"Backspace":  "backspace",
-		"Tab":        "tab",
-		" ":          "space",
-		"ArrowUp":    "up",
-		"ArrowDown":  "down",
-		"ArrowLeft":  "left",
-		"ArrowRight": "right",
-		"Shift":      "shift",
-		"Control":    "ctrl",
-		"Alt":        "alt",
-		"CapsLock":   "capslock",
-	}
 
 	// Map (F1 - F12) keys
 	if strings.HasPrefix(jsKey, "F") && len(jsKey) > 1 {
