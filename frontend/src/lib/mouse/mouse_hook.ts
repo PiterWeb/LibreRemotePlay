617ba0edc4ff2263c1884ca2d2ac3d4d7fe77cd3
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2025-12-18 20:53:10 +0100

    frontend mouse hook modified to use arraybuffers

---
+++ frontend/src/lib/mouse/mouse_hook.ts
@@ -0,0 +1,67 @@
+enum MouseType {
+	Click = 0,
+	Move = 1
+}
+
+enum MouseValue {
+	Left = 0,
+	Central = 1,
+	Right = 2
+}
+
+type mouseHandler = (output: ArrayBuffer) => void;
+
+const click_events: (keyof HTMLVideoElementEventMap)[] = ['click', 'auxclick'];
+
+export function handleClick(callback: mouseHandler) {
+	const handler = (event: Event) => {
+		if (!(event instanceof PointerEvent)) return;
+		console.log(`Click ${event.type}: ${event.button}`);
+
+		const value = event.button as MouseValue;
+		
+		const buf = new ArrayBuffer(2)
+		const view = new Uint8Array(buf);
+		
+		view[0] = MouseType.Click
+		view[1] = value 
+		
+		return callback(buf);
+	};
+
+	click_events.forEach((event_name) => document?.addEventListener(event_name, handler, true));
+
+	return handler;
+}
+
+export function unhandleClick(callback: ReturnType<typeof handleClick>) {
+	click_events.forEach((event_name) => document?.removeEventListener(event_name, callback, true));
+}
+
+export function handleMove(callback: mouseHandler) {
+	const handler = (event: Event) => {
+		if (!(event instanceof MouseEvent)) return;
+
+    const xAxis = event.pageX;
+    const yAxis = event.pageY;
+		console.log(`Move x:${xAxis}, y:${yAxis}`);
+		
+		const buf = new ArrayBuffer(2)
+		const view = new DataView(buf);
+		
+		view.setUint8(0, MouseType.Move)
+		view.setBigUint64(1, BigInt(xAxis))
+		view.setBigUint64(2 + 8 * 8, BigInt(yAxis))
+		
+		return callback(buf);
+	};
+
+	document?.addEventListener("mousemove", handler, true);
+
+	return handler;
+}
+
+export function unhandleMove(callback: ReturnType<typeof handleClick>) {
+  document?.removeEventListener("mousemove", callback, true);
+}
+
