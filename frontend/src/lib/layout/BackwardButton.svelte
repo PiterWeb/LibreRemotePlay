414fe29bbcab957277ec6de8593cb444633dae69
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2026-02-13 20:24:07 +0100

    BackwardButton now has 'absolute' position instead of 'fixed'

--- frontend/src/lib/layout/BackwardButton.svelte
+++ frontend/src/lib/layout/BackwardButton.svelte
@@ -15,7 +15,7 @@
 	
 </script>
 
-<a aria-label="go back" id="tutorial-back-btn" href={path} class="fixed top-28 md:right-28 right-20 p-3 rounded-full bg-white shadow-lg">
+<a aria-label="go back" id="tutorial-back-btn" href={path} class="absolute top-28 md:right-28 right-20 p-3 rounded-full bg-white shadow-lg">
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		class="h-6 w-6"
